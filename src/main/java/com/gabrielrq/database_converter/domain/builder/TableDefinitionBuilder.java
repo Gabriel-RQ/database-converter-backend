3905efa589a9eb6697df37bf79a12c618a161da4
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-10-07 17:23:47 -0300

    feat: adds TableDefinitionBuilder

---
+++ src/main/java/com/gabrielrq/database_converter/domain/builder/TableDefinitionBuilder.java
@@ -0,0 +1,78 @@
+package com.gabrielrq.database_converter.domain.builder;
+
+import com.gabrielrq.database_converter.domain.ColumnDefinition;
+import com.gabrielrq.database_converter.domain.ForeignKeyDefinition;
+import com.gabrielrq.database_converter.domain.TableDefinition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableDefinitionBuilder {
+    private String name;
+    private List<ColumnDefinition> columns = new ArrayList<>();
+    private List<String> primaryKeyColumns = new ArrayList<>();
+    private List<ForeignKeyDefinition> foreignKeys = new ArrayList<>();
+    private List<List<String>> uniqueConstraints = new ArrayList<>();
+    private String schema;
+
+    public TableDefinitionBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public TableDefinitionBuilder setColumns(List<ColumnDefinition> columns) {
+        this.columns = columns;
+        return this;
+    }
+
+    public TableDefinitionBuilder addColumn(ColumnDefinition column) {
+        this.columns.add(column);
+        return this;
+    }
+
+    public TableDefinitionBuilder setPrimaryKeyColumns(List<String> primaryKeyColumns) {
+        this.primaryKeyColumns = primaryKeyColumns;
+        return this;
+    }
+
+    public TableDefinitionBuilder addPrimaryKeyColumn(String columnName) {
+        this.primaryKeyColumns.add(columnName);
+        return this;
+    }
+
+    public TableDefinitionBuilder setForeignKeys(List<ForeignKeyDefinition> foreignKeys) {
+        this.foreignKeys = foreignKeys;
+        return this;
+    }
+
+    public TableDefinitionBuilder addForeignKey(ForeignKeyDefinition fk) {
+        this.foreignKeys.add(fk);
+        return this;
+    }
+
+    public TableDefinitionBuilder setUniqueConstraints(List<List<String>> uniqueConstraints) {
+        this.uniqueConstraints = uniqueConstraints;
+        return this;
+    }
+
+    public TableDefinitionBuilder addUniqueConstraint(List<String> constraint) {
+        this.uniqueConstraints.add(constraint);
+        return this;
+    }
+
+    public TableDefinitionBuilder setSchema(String schema) {
+        this.schema = schema;
+        return this;
+    }
+
+    public TableDefinition build() {
+        return new TableDefinition(
+                name,
+                columns,
+                primaryKeyColumns,
+                foreignKeys,
+                uniqueConstraints,
+                schema
+        );
+    }
+}
