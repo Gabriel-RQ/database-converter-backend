f333e971cd5654303972b75de102b35463c6cc8e
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-10-10 10:52:01 -0300

    update(DataTransformationService): adds 'mapTargetTypes' and 'generateSQL' methods

--- src/main/java/com/gabrielrq/database_converter/service/etl/DataTransformationService.java
+++ src/main/java/com/gabrielrq/database_converter/service/etl/DataTransformationService.java
@@ -5,6 +5,7 @@ import com.gabrielrq.database_converter.domain.builder.ColumnDefinitionBuilder;
 import com.gabrielrq.database_converter.domain.builder.DatabaseDefinitionBuilder;
 import com.gabrielrq.database_converter.domain.builder.TableDefinitionBuilder;
 import com.gabrielrq.database_converter.service.JsonService;
+import com.gabrielrq.database_converter.service.SqlService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,31 +17,42 @@ import java.util.Map;
 public class DataTransformationService {
 
     private final JsonService jsonService;
+    private final SqlService sqlService;
 
-    public DataTransformationService(JsonService jsonService) {
+    public DataTransformationService(JsonService jsonService, SqlService sqlService) {
         this.jsonService = jsonService;
+        this.sqlService = sqlService;
+    }
+
+    private DatabaseDefinition mapTargetTypes(DatabaseDefinition originMetadata, Map<Integer, String> targetConversionMap) {
+        DatabaseDefinitionBuilder databaseBuilder = DatabaseDefinitionBuilder.from(originMetadata).setTables(new ArrayList<>());
+        for (var table : originMetadata.tables()) {
+            TableDefinitionBuilder tableBuilder = TableDefinitionBuilder.from(table).setColumns(new ArrayList<>());
+            for (var column : table.columns()) {
+                tableBuilder.addColumn(
+                        ColumnDefinitionBuilder
+                                .from(column)
+                                .setTargetType(targetConversionMap.getOrDefault(column.genericType(), "INVALID"))
+                                .build()
+                );
+            }
+            databaseBuilder.addTable(tableBuilder.build());
+        }
+        return databaseBuilder.build();
+    }
+
+    private void generateSQL(DatabaseDefinition metadata) {
+        sqlService.generateDDL(metadata);
     }
 
     public DatabaseDefinition transform(DatabaseDefinition metadata, String target) {
         try {
             Map<Integer, String> targetConversioMap = jsonService.readConversionMap(target);
-
-            DatabaseDefinitionBuilder databaseBuilder = DatabaseDefinitionBuilder.from(metadata).setTables(new ArrayList<>());
-            for (var table : metadata.tables()) {
-                TableDefinitionBuilder tableBuilder = TableDefinitionBuilder.from(table).setColumns(new ArrayList<>());
-                for (var column : table.columns()) {
-                    tableBuilder.addColumn(
-                            ColumnDefinitionBuilder
-                                    .from(column)
-                                    .setTargetType(targetConversioMap.getOrDefault(column.genericType(), "INVALID"))
-                                    .build()
-                    );
-                }
-                databaseBuilder.addTable(tableBuilder.build());
-            }
-            var targetMetadata = databaseBuilder.build();
+            var targetMetadata = mapTargetTypes(metadata, targetConversioMap);
             Path outputPath = Path.of(metadata.name());
             jsonService.write(targetMetadata, outputPath.resolve("target.meta").toString());
+
+            generateSQL(targetMetadata);
 
             return targetMetadata;
         } catch (IOException e) {
