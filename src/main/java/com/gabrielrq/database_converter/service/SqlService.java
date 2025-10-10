9862e7dcacb76ad1dd8014a561a44299f6bfdc0e
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-10-10 15:21:10 -0300

    refactor(SqlService): extracts smaller methods from 'generateDDL' to enhance maintenance

--- src/main/java/com/gabrielrq/database_converter/service/SqlService.java
+++ src/main/java/com/gabrielrq/database_converter/service/SqlService.java
@@ -1,6 +1,7 @@
 package com.gabrielrq.database_converter.service;
 
 import com.gabrielrq.database_converter.domain.DatabaseDefinition;
+import com.gabrielrq.database_converter.domain.TableDefinition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +31,7 @@ public class SqlService {
     }
 
     public void generateDML(DatabaseDefinition metadata) {
-        // TODO
+
     }
 
     public void generateDDL(DatabaseDefinition metadata) {
@@ -40,69 +41,81 @@ public class SqlService {
         Path outDir = Path.of(basePath).resolve(metadata.name()).resolve(ddlPath);
 
         for (var table : metadata.tables()) {
-            StringBuilder ddlBuilder = new StringBuilder();
-            List<String> columnDefinitions = new ArrayList<>();
-            for (var column : table.columns()) {
-                StringBuilder columnDef = new StringBuilder();
-                columnDef
-                        .append(column.name())
-                        .append(" ")
-                        .append(column.targetType());
-                switch (column.genericType()) {
-                    case Types.VARCHAR, Types.NVARCHAR, Types.LONGVARCHAR, Types.LONGNVARCHAR ->
-                            columnDef.append("(").append(column.length()).append(")");
-                    case Types.NUMERIC, Types.DECIMAL ->
-                            columnDef.append("(").append(column.precision()).append(",").append(column.scale()).append(")");
-
-                }
-                columnDefinitions.add("\t" + columnDef);
-            }
-
-            ddlBuilder
+            StringBuilder ddlBuilder = new StringBuilder()
                     .append("CREATE TABLE ")
                     .append(table.schema())
                     .append(".")
                     .append(table.name())
-                    .append(" (\n")
-                    .append(String.join(",\n", columnDefinitions));
+                    .append(" (\n");
 
-            if (!table.primaryKeyColumns().isEmpty()) {
-                ddlBuilder
-                        .append(",\n\t")
-                        .append("PRIMARY KEY (").append(String.join(",", table.primaryKeyColumns())).append(")");
-            }
-
-            List<String> fkDefinitions = new ArrayList<>();
-            if (!table.foreignKeys().isEmpty()) {
-                ddlBuilder.append(",\n");
-
-                for (var fk : table.foreignKeys()) {
-                    String fkDef = "FOREIGN KEY (" +
-                            String.join(",", fk.localColumns()) +
-                            ") REFERENCES " +
-                            fk.referencedTable() +
-                            " (" +
-                            String.join(",", fk.referencedColumns()) +
-                            ")";
-                    fkDefinitions.add("\t" + fkDef);
-                }
-            }
-            ddlBuilder.append(String.join(",\n", fkDefinitions));
-
-
-            List<String> uniqueDefinitions = new ArrayList<>();
-            if (!table.uniqueConstraints().isEmpty()) {
-                ddlBuilder.append(",\n");
-                for (var unique : table.uniqueConstraints()) {
-                    String uniqueDef = "UNIQUE " + "(" + String.join(",", unique) + ")";
-                    uniqueDefinitions.add("\t" + uniqueDef);
-                }
-            }
-            ddlBuilder.append(String.join(",\n", uniqueDefinitions));
+            generateDDLColumn(table, ddlBuilder);
+            generateDDLPk(table, ddlBuilder);
+            generateDDLFk(table, ddlBuilder);
+            generateDDLUnique(table, ddlBuilder);
 
             ddlBuilder.append("\n);");
             write(outDir.resolve(table.schema() + "." + table.name() + ".sql"), ddlBuilder.toString());
         }
+    }
+
+    private void generateDDLUnique(TableDefinition table, StringBuilder ddlBuilder) {
+        List<String> uniqueDefinitions = new ArrayList<>();
+        if (!table.uniqueConstraints().isEmpty()) {
+            ddlBuilder.append(",\n");
+            for (var unique : table.uniqueConstraints()) {
+                String uniqueDef = "UNIQUE " + "(" + String.join(",", unique) + ")";
+                uniqueDefinitions.add("\t" + uniqueDef);
+            }
+        }
+        ddlBuilder.append(String.join(",\n", uniqueDefinitions));
+    }
+
+    private void generateDDLFk(TableDefinition table, StringBuilder ddlBuilder) {
+        List<String> fkDefinitions = new ArrayList<>();
+        if (!table.foreignKeys().isEmpty()) {
+            ddlBuilder.append(",\n");
+
+            for (var fk : table.foreignKeys()) {
+                String fkDef = "FOREIGN KEY (" +
+                        String.join(",", fk.localColumns()) +
+                        ") REFERENCES " +
+                        fk.referencedTable() +
+                        " (" +
+                        String.join(",", fk.referencedColumns()) +
+                        ")";
+                fkDefinitions.add("\t" + fkDef);
+            }
+        }
+        ddlBuilder.append(String.join(",\n", fkDefinitions));
+    }
+
+    private void generateDDLPk(TableDefinition table, StringBuilder ddlBuilder) {
+        if (!table.primaryKeyColumns().isEmpty()) {
+            ddlBuilder
+                    .append(",\n\t")
+                    .append("PRIMARY KEY (").append(String.join(",", table.primaryKeyColumns())).append(")");
+        }
+    }
+
+    private void generateDDLColumn(TableDefinition table, StringBuilder ddlBuilder) {
+        List<String> columnDefinitions = new ArrayList<>();
+        for (var column : table.columns()) {
+            StringBuilder columnDef = new StringBuilder();
+            columnDef
+                    .append(column.name())
+                    .append(" ")
+                    .append(column.targetType());
+            switch (column.genericType()) {
+                case Types.VARCHAR, Types.NVARCHAR, Types.LONGVARCHAR, Types.LONGNVARCHAR ->
+                        columnDef.append("(").append(column.length()).append(")");
+                case Types.NUMERIC, Types.DECIMAL ->
+                        columnDef.append("(").append(column.precision()).append(",").append(column.scale()).append(")");
+
+            }
+            columnDefinitions.add("\t" + columnDef);
+        }
+
+        ddlBuilder.append(String.join(",\n", columnDefinitions));
     }
 
     public void write(Path path, String content) {
